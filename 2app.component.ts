90126c7dfb1a79b2b73377d3e52a053267d39c2a
jnjijij <136825012+jnjijij@users.noreply.github.com>
2023-12-08 22:29:50 +0200

    Create 2app.component.ts

---
+++ 2app.component.ts
@@ -0,0 +1,18 @@
+// app.component.ts
+import { Component } from '@angular/core';
+import { CarService } from './car.service';
+
+@Component({
+    selector: 'app-root',
+    template: `...`,
+    styles: [
+        `
+      .car-item {
+        margin-bottom: 20px;
+      }
+    `,
+    ],
+})
+export class AppComponent {
+    // ... залиште інший код без змін
+}
