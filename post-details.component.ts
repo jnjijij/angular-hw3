f890a1a24ff277d97c1dfd4f0997260fa18f0d39
jnjijij <136825012+jnjijij@users.noreply.github.com>
2023-12-08 22:40:20 +0200

    Create post-details.component.ts

---
+++ post-details.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+    selector: 'app-post-details',
+    templateUrl: './post-details.component.html',
+    styleUrls: ['./post-details.component.css']
+})
+export class PostDetailsComponent implements OnInit {
+    post: any;
+
+    constructor(private route: ActivatedRoute) { }
+
+    ngOnInit() {
+        this.post = history.state.post;
+    }
+}
